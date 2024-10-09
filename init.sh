f600d5fb3fccf11e55068c29d8055f044a9f313b
mateja-velickovic <mateja.velickovic@eduvaud.ch>
2024-10-09 20:48:52 +0200

    fix(sh): Correction d'un bug mineur sur le script de lancement

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker stop pma db web && docker rm pma db web && \
-docker volume rm web-educational_app_dbdata pprod_dbdata
+docker volume rm web-educational_app_dbdata pprod_dbdata educational_dbdata
 docker image rm matap
 docker build -t matap .
 docker compose up -d
