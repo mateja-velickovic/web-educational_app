ae1b01e1f5174bf877c747ffa63c352fd6f2e8b7
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2024-11-01 10:41:25 +0100

    fix(init.sh): Arrêt du script si Docker n'est pas en cours d'exécution

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if ! docker info > /dev/null 2>&1; then
+    echo "Docker n'est pas lancé. Veuillez démarrer Docker et réessayer."
+    exit 1
+fi
+
 docker stop pma db web && docker rm pma db web && \
 docker volume rm web-educational_app_dbdata pprod_dbdata educational_dbdata
 docker image rm matap
