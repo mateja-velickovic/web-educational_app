5bbf7bdbc61616cb7f06df0840e246a1350f20fc
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2024-11-01 11:34:11 +0100

    fix(db): Mise à jour de la chaîne de connexion

--- p_prod-jp/src/php/lib/database.php
+++ p_prod-jp/src/php/lib/database.php
@@ -6,7 +6,7 @@ $db = "db_jpprod";
 $user = "matvelickov";
 $password = getenv('DB_PASSWORD');
 
-$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=UTF8";
 
 $pdo = new PDO($dsn, $user, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
