9dc5f66388ee294f670af1a233a95ffb68bf11a5
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2024-12-12 08:14:38 +0100

    fix(script): Optimisation du script

--- init.sh
+++ init.sh
@@ -5,35 +5,20 @@ RED='\033[0;31m'  # Rouge
 GREEN='\033[0;32m' # Vert
 NC='\033[0m'      # Blanc
 
-# Affichage du pourcentage
-show_percentage() {
-    local progress=0
-    while [ "$progress" -le 100 ]; do
-        printf "\rMise en place de l'environnement Docker : %d%%" "$progress"
-        sleep 0.1
-        progress=$((progress + 1))
-    done
-    echo
-}
-
 # Vérifier si Docker est lancé
 if ! docker info > /dev/null 2>&1; then
     echo -e "${RED}>> Docker n'est pas lancé. Veuillez démarrer Docker et réessayer.${NC}"
     exit 1
 fi
 
-# Mise en place de l'environnement sans sortie dans la console
-{
-    docker stop pma db web > /dev/null 2>&1
-    docker rm pma db web > /dev/null 2>&1
-    docker volume rm web-educational_app_dbdata > /dev/null 2>&1
-    docker image rm matap:edu.1.0 > /dev/null 2>&1
-    docker build -t matap:edu.1.0 . > /dev/null 2>&1
-    docker compose up -d > /dev/null 2>&1
-} &
+echo -e "${NC}Mise en place de l'environnement..."
 
-# Afficher la progression pendant les opérations
-show_percentage
+docker stop pma db web > /dev/null 2>&1
+docker rm pma db web > /dev/null 2>&1
+docker volume rm web-educational_app_dbdata > /dev/null 2>&1
+docker image rm matap:edu.1.0 > /dev/null 2>&1
+docker build -t matap:edu.1.0 . > /dev/null 2>&1
+docker compose up -d > /dev/null 2>&1
 
 localhost="http://localhost:8080"
 
